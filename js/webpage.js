57c5551267133877de5956e30c60cd11788e0b1b
Anton <antonliann@gmail.com>
2021-01-13 15:59:15 +0200

    Removed useless JS code and rewrote it to CSS

--- js/webpage.js
+++ js/webpage.js
@@ -2,41 +2,16 @@ function recount() {
 
   if (window.matchMedia('screen and (min-width: 1215px)').matches) {
     document.body.style.zoom = "1";
-    for (let i = 1; i < 10; i++) {
-      if (document.getElementById('desktop' + i))
-        document.getElementById('desktop' + i).style.display = "unset";
-      if (document.getElementById('tablet' + i))
-        document.getElementById('tablet' + i).style.display = "none";
-      if (document.getElementById('phone' + i))
-        document.getElementById('phone' + i).style.display = "none"
-    }
   }
 
-  if (window.matchMedia('screen and (min-width: 834px) and (max-width: 1214px), screen and (max-width: 833px) and (orientation: landscape)').matches) {
-    if (window.matchMedia('(max-width: 860px) and (orientation: landscape)').matches)
+  if (window.matchMedia('screen and (min-width: 500px) and (max-width: 1214px), screen and (max-width: 833px) and (orientation: landscape)').matches) {
+    if (window.matchMedia('(max-width: 860px)').matches)
       document.body.style.zoom = (window.innerWidth / 880).toString();
     else document.body.style.zoom = "1";
-    for (let j = 1; j < 10; j++) {
-      if (document.getElementById('desktop' + j))
-        document.getElementById('desktop' + j).style.display = "none";
-      if (document.getElementById('tablet' + j))
-        document.getElementById('tablet' + j).style.display = "unset";
-      if (document.getElementById('phone' + j))
-        document.getElementById('phone' + j).style.display = "none";
-    }
   }
 
-  if (window.matchMedia('screen and (max-width: 833px) and (orientation: portrait)').matches) {
+  if (window.matchMedia('screen and (max-width: 499px) and (orientation: portrait)').matches) {
     document.body.style.zoom = (window.innerWidth / 375).toString();
-    document.getElementById('bus-photo').style.display = "none";
-    for (let k = 1; k < 10; k++) {
-      if (document.getElementById('desktop' + k))
-        document.getElementById('desktop' + k).style.display = "none";
-      if (document.getElementById('tablet' + k))
-        document.getElementById('tablet' + k).style.display = "none";
-      if (document.getElementById('phone' + k))
-        document.getElementById('phone' + k).style.display = "unset";
-    }
   }
 }
 
