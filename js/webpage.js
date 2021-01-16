af9ec86dd2b35dd81c01c5c75fe7252d4e48604f
Anton <antonliann@gmail.com>
2021-01-16 20:33:43 +0200

    Cross browser bug fixing

--- js/webpage.js
+++ js/webpage.js
@@ -1,7 +1,6 @@
 function recount() {
-
-  if (window.matchMedia('(min-width: 1215px)').matches) {
-    document.body.style.zoom = "1";
+  if (window.matchMedia('(max-width: 499px) and (orientation: portrait)').matches) {
+    document.body.style.zoom = ((window.innerWidth) / 375).toString();
   }
 
   if (window.matchMedia('(min-width: 500px) and (max-width: 1214px), (max-width: 833px) and (orientation: landscape)').matches) {
@@ -10,8 +9,8 @@ function recount() {
     else document.body.style.zoom = "1";
   }
 
-  if (window.matchMedia('(max-width: 499px) and (orientation: portrait)').matches) {
-    document.body.style.zoom = ((window.innerWidth) / 375).toString();
+  if (window.matchMedia('(min-width: 1215px)').matches) {
+    document.body.style.zoom = "1";
   }
 }
 
