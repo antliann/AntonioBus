37a11b3dde7bc275f2f2202196a5b00c75b14d12
Anton <antonliann@gmail.com>
2021-01-17 23:46:07 +0200

    Hamburger menu animation added

--- js/webpage.js
+++ js/webpage.js
@@ -1,29 +1,24 @@
 let conditions = {
   isChangedFromNormal: true,
+  menuIsOpenedPhone: false,
   firefoxBrowser: (navigator.userAgent.indexOf("Firefox") > -1),
-  menuIsOpenedPhone: false
 };
 
 function recount() {
-
   if (window.matchMedia('(max-width: 499px) and (orientation: portrait)').matches) {
     document.body.style.zoom = ((window.innerWidth) / 375).toString();
   }
-
   if (window.matchMedia('(min-width: 500px) and (max-width: 1214px), ' +
     '(max-width: 833px) and (orientation: landscape)').matches) {
     if (window.matchMedia('(max-width: 860px)').matches)
       document.body.style.zoom = (window.innerWidth / 880).toString();
-    else {
+    else
       document.body.style.zoom = "1";
-    }
   }
-
   if (window.matchMedia('(min-width: 1215px)').matches) {
     document.body.style.zoom = "1";
     conditions.isChangedFromNormal = true;
   }
-
   if (conditions.isChangedFromNormal && conditions.firefoxBrowser && window.matchMedia('(max-width: 860px)').matches) {
     alert("При вашем разрешении экрана содержимое в браузере может отображаться некорректно, " +
       "для лучшего опыта воспользуйтесь, пожалуйста, другим браузером");
@@ -41,11 +36,53 @@ window.addEventListener('orientationchange', function () {
   recount();
 });
 
+let bar1 = document.getElementById("menu-bar-1");
+let bar2 = document.getElementById("menu-bar-2");
+let bar3 = document.getElementById("menu-bar-3");
+
+function middleState() {
+  bar1.style.width = "36px";
+  bar2.style.width = "36px";
+  bar3.style.width = "36px";
+  bar1.style.transform = "translateX(-14px) translateY(10px) rotate(-90deg)";
+  bar2.style.transform = "rotate(-90deg)";
+  bar3.style.transform = "translateX(14px) translateY(-10px) rotate(-90deg)";
+  bar2.style.background = "rgba(255, 255, 255, 0.5)";
+}
+
+function hamburger() {
+  setTimeout(function () {
+    bar1.style.transform = "";
+    bar1.style.width = "";
+    bar2.style.transform = "";
+    bar2.style.width = "";
+    bar2.style.background = "";
+    bar3.style.transform = "";
+    bar3.style.width = "";
+  }, 140);
+}
+
+function cross() {
+  setTimeout(function () {
+    bar1.style.transform = "translateY(10px) rotate(-45deg)";
+    bar1.style.width = "40px";
+    bar2.style.width = "40px";
+    bar2.style.background = "transparent";
+    bar3.style.transform = "translateY(-10px) rotate(-135deg)";
+    bar3.style.width = "40px";
+  }, 140);
+}
+
 document.getElementById('hamburger-button').addEventListener('click', function () {
-  if (conditions.menuIsOpenedPhone)
+  middleState();
+  if (conditions.menuIsOpenedPhone){
     document.getElementById('menu-backgr').style.left = "-100%";
-  else
+    hamburger();
+  }
+  else {
     document.getElementById('menu-backgr').style.left = "0";
+    cross();
+  }
   conditions.menuIsOpenedPhone = !(conditions.menuIsOpenedPhone);
 })
 
@@ -54,15 +91,14 @@ document.getElementById("subscribe").addEventListener('click', function () {
   if (emailField.value !== "") {
     emailField.style.transform = "scale(0.8)";
     emailField.style.transition = "200ms";
-    setTimeout(
-      function () {
+    setTimeout(function () {
         if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-][a-zA-Z0-9-]+)*$/.test(emailField.value)) {
           emailField.value = "";
           emailField.placeholder = "Отлично! Проверь почту!";
         } else {
           emailField.style.color = "#ff0000";
           emailField.addEventListener('input', function () {
-            emailField.style.color = "#343434";
+            emailField.style.color = "";
           })
         }
         emailField.style.transform = "scale(1.0)";
